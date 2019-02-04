879f0b69c92aed2867147de83e763a19bfb015bb
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 11:18:21 +0100

    Fix string quote missing

--- ci/install_win.ps1
+++ ci/install_win.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
 Set-PSDebug -Trace 1
-$Version = 6.1.2
+$Version = '6.1.2'
 Invoke-WebRequest -Out $Home\AppData\Local\Temp -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
 # TODO
