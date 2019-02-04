95c122506a7c328e67c324002044f48a05c27287
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 11:39:09 +0100

    Try fix Windows CI

--- ci/install_win.ps1
+++ ci/install_win.ps1
@@ -1,5 +1,8 @@
 $ErrorActionPreference = 'Stop'
 Set-PSDebug -Trace 1
 $Version = '6.1.2'
-Invoke-WebRequest -Out $Home\AppData\Local\Temp -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
-# TODO
+$TempFile = (New-TemporaryFile).FullName
+Invoke-WebRequest -Out $TempFile -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
+Expand-Archive -Path $TempFile -Destination "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
+Remove-Item $TempFile -Force
+New-Item -ItemType SymbolicLink -Path "$($Env:SystemRoot)\pwsh.exe" -Value "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version\pwsh.exe"
