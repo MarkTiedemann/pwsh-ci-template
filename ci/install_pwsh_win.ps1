2ef15cf52e29cf3ac98661643cf3069e601e6753
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 12:35:22 +0100

    Fix missing string quotes

--- ci/install_pwsh_win.ps1
+++ ci/install_pwsh_win.ps1
@@ -5,7 +5,7 @@ $Dir = "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
 Invoke-WebRequest -Out $Zip -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
 Write-Host $Dir
 Expand-Archive $Zip -Destination $Dir
-ls C:\Program Files\PowerShell\6
-ls C:\Program Files\PowerShell\6.1.2
+ls 'C:\Program Files\PowerShell\6'
+ls 'C:\Program Files\PowerShell\6.1.2'
 Remove-Item $Zip -Force
 [Environment]::SetEnvironmentVariable('Path', "$($Env:Path);$Dir", [EnvironmentVariableTarget]::User)
