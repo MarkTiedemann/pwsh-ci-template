0b1c27a2aa04c0c0996bef32112cca1749b78bd4
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 11:45:38 +0100

    Debug Windows CI

--- ci/install_win.ps1
+++ ci/install_win.ps1
@@ -2,5 +2,6 @@ $ErrorActionPreference = 'Stop'
 $Version = '6.1.2'
 Invoke-WebRequest -Out "$($Env:Temp)\powershell.zip" -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
 Expand-Archive -Path "$($Env:Temp)\powershell.zip" -Destination "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
+ls "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
 Remove-Item "$($Env:Temp)\powershell.zip" -Force
 New-Item -ItemType SymbolicLink -Path "$($Env:SystemRoot)\pwsh.exe" -Value "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version\pwsh.exe"
