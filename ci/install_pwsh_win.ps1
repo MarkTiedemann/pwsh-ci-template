7036fc9673e38e7a8434cd7a40f49b1f27f25081
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 12:54:29 +0100

    Debug Windows CI further

--- ci/install_pwsh_win.ps1
+++ ci/install_pwsh_win.ps1
@@ -3,9 +3,7 @@ $Version = '6.1.2'
 $Zip = "$($Env:Temp)\powershell.zip"
 $Dir = "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
 Invoke-WebRequest -Out $Zip -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
-Write-Host $Dir
-Expand-Archive $Zip -Destination $Dir
 ls 'C:\Program Files\PowerShell\6'
-ls 'C:\Program Files\PowerShell\6.1.2'
+Expand-Archive $Zip -Destination $Dir
 Remove-Item $Zip -Force
 [Environment]::SetEnvironmentVariable('Path', "$($Env:Path);$Dir", [EnvironmentVariableTarget]::User)
