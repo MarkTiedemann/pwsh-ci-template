eb2690e662aa2ff3ce06abad5b89187ad44f9516
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 11:12:50 +0100

    Try to fix CI

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -2,5 +2,5 @@
 set -ev
 version=6.1.2
 curl -L -o /tmp/powershell.deb https://github.com/PowerShell/PowerShell/releases/download/v$version/powershell_$version-1.ubuntu.14.04_amd64.deb
-sudo dpkg -i /tmp/powershell.deb
+sudo dpkg -i --force-depends /tmp/powershell.deb
 rm /tmp/powershell.deb
