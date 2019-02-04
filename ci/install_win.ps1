e81ed995db3f559f9b2067cdbde637a5e88e9ecf
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 12:01:08 +0100

    Add pwsh to Path instead of symlinking

--- ci/install_win.ps1
+++ ci/install_win.ps1
@@ -1,7 +1,8 @@
 $ErrorActionPreference = 'Stop'
 $Version = '6.1.2'
-Invoke-WebRequest -Out "$($Env:Temp)\powershell.zip" -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
-Expand-Archive -Path "$($Env:Temp)\powershell.zip" -Destination "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
-ls "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
-Remove-Item "$($Env:Temp)\powershell.zip" -Force
-New-Item -ItemType SymbolicLink -Path "$($Env:SystemRoot)\pwsh.exe" -Value "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version\pwsh.exe"
+$Zip = "$($Env:Temp)\powershell.zip"
+$Dir = "$($Env:ProgramFiles)\Microsoft\PowerShell\$Version"
+Invoke-WebRequest -Out $Zip -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
+Expand-Archive $Zip -Destination $Dir
+Remove-Item $Zip -Force
+[Environment]::SetEnvironmentVariable('Path', "$($Env:Path);$Dir", [EnvironmentVariableTarget]::User)
