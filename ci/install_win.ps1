b3cb9037e987a0c5dccbcb53776dbdff56992afd
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-02-04 11:14:33 +0100

    Fix typo

--- ci/install_win.ps1
+++ ci/install_win.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
 Set-PSDebug -Trace 1
 $Version = 6.1.2
-Invoke-WebRequest -Out $Home\AppData\Local\Temp -Uri https://github.com/PowerShell/PowerShell/releases/download/v$version/PowerShell-$version-win-x64.zip
+Invoke-WebRequest -Out $Home\AppData\Local\Temp -Uri https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.zip
 # TODO
